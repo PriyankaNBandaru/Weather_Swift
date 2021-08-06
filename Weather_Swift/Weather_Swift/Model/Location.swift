ac6237315b13e80e63497975a8a1b3a91f274cc7
PriyankaNBandaru <61023973+PriyankaNBandaru@users.noreply.github.com>
2021-08-06 17:37:48 -0400

    Created and uploaded the files

---
+++ Weather_Swift/Weather_Swift/Model/Location.swift
@@ -0,0 +1,17 @@
+//
+//  Location.swift
+//  Weather_Swift
+//
+//  Created by Priyanka Bandaru on 8/3/21.
+//
+
+import UIKit
+
+class Location: NSObject{
+    
+    var woeid: Int?
+    var title: String?
+    var location_type:String?
+    var locationWeathers:LocationWeather?
+}
+
---
+++ Weather_Swift/Weather_Swift/Model/LocationWeather.swift
@@ -0,0 +1,24 @@
+//
+//  LocationWeather.swift
+//  Weather_Swift
+//
+//  Created by Priyanka Bandaru on 8/4/21.
+//
+
+import UIKit
+
+class LocationWeather: NSObject {
+    var weatherStateAbbr:String?
+    var weatherState:String?
+    var min_Temp:Double?
+    var max_Temp:Double?
+    var applicable_date:String?
+    var wind_direction_compass:String?
+    var the_temp:Double?
+    var wind_speed:Double?
+    var wind_direction: Double?
+    var air_pressure:Double?
+    var humidity:Double?
+    var visibility:Double?
+    var predictability:Int?
+}
